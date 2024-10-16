96db73d86a220d5ff8eec85b158b9d31418085c6
phosphorus9273 <phosphorus158@gmail.com>
2024-10-17 06:54:34 +0800

    Update app.3.js

--- app.3.js
+++ app.3.js
@@ -274,9 +274,10 @@ function getElements(kb, storeInDefault) {
         
             await Promise.any(fetches);
         
-            if (myServer && myServer.children[0] && myServer.children[0].onclick) {
+            /*if (myServer && myServer.children[0] && myServer.children[0].onclick) {
                 myServer.children
-            }        
+            }*/
+    }
     // Save forms
     util.retrieveFromLocalStorage("playerNameInput");
     util.retrieveFromLocalStorage("playerKeyInput");
